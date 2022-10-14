95ea445f06707d78f371b2c343171aa31e67c2f7
mlanfran <lanfranconi@gmail.com>
2022-10-14 11:46:07 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,71 @@
+const menuEmail = document.querySelector('.navbar-email')
+const desktopMenu = document.querySelector('.desktop-menu')
+const cardsContainer = document.querySelector('.cards-container')
+
+menuEmail.addEventListener('click', toogleDesktopMenu)
+
+function toogleDesktopMenu(){
+    desktopMenu.classList.toggle('inactive')
+}
+
+
+
+const productList = []
+productList.push({
+    name : 'bike',
+    price : '$ 120',
+    image : 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+
+})
+productList.push({
+    name : 'book',
+    price : '$ 20',
+    image : 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+
+})
+productList.push({
+    name : 'tv',
+    price : '$ 320',
+    image : 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+
+})
+
+function renderProductList(arr){
+    for (product of arr){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+
+        const img = document.createElement('img');
+        img.setAttribute('src', product.image);
+
+        
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const productInfoDiv = document.createElement('div');
+        const productPrice = document.createElement('p');
+        const productName = document.createElement('p');
+        productPrice.innerText = product.price;
+        productName.innerText = product.name;
+
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+        productInfoFigure.appendChild(productImgCart);
+
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+
+        productCard.appendChild(img);
+        productCard.appendChild(productInfo);
+
+        cardsContainer.appendChild(productCard);
+
+    }
+}
+
+renderProductList(productList);
